c686d4abe8f4d05aba7edfdf209f7f050057588f
Michael Goldfarb <michael.goldfarb@gmail.com>
2013-04-28 21:44:30 -0400

    Updated the kill script to run all ssh command in background

--- grinderscripts/kill_tests_ecelinux.sh
+++ grinderscripts/kill_tests_ecelinux.sh
@@ -4,8 +4,10 @@
 for h in $(get_live_ecelinux_hosts.sh)
 do
 		echo "Killing grinder processed on $h"
-		CMDSTR="killall grinder;"
-		ssh -o StrictHostKeyChecking=no mgoldfar@$h "$CMDSTR"
-done
+		CMDSTR="killall java;"
+		ssh -o StrictHostKeyChecking=no mgoldfar@$h "$CMDSTR" & 
+done &
+
+wait $!
 
 exit 0
