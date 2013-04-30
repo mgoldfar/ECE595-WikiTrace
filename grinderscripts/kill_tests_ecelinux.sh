6dc8e982be975aaa3681d645222bb49ca26170cd
Michael Goldfarb <michael.goldfarb@gmail.com>
2013-04-29 20:25:13 -0400

    Surpress error in kill test script

--- grinderscripts/kill_tests_ecelinux.sh
+++ grinderscripts/kill_tests_ecelinux.sh
@@ -3,7 +3,7 @@
 # get NUMHOSTS live hosts
 for h in $(get_live_ecelinux_hosts.sh)
 do
-		echo "Killing grinder processed on $h"
+		echo "Killing grinde 2>/dev/nullr processed on $h"
 		CMDSTR="killall java;"
 		ssh -o StrictHostKeyChecking=no mgoldfar@$h "$CMDSTR" & 
 done &
