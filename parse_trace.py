e77738af0a8392ebf69850931e22103ce421f8a7
Michael Goldfarb <michael.goldfarb@gmail.com>
2013-04-30 23:54:39 -0400

    Added download only option to trace parser

--- parse_trace.py
+++ parse_trace.py
@@ -8,13 +8,14 @@ import StringIO
 import progressbar
 
 if len(sys.argv) <= 3:
-	sys.stderr.write("usage: parse_trace.py <trace id> <start> <end> [--trace_archive_dir=traces] [--trace_server=localhost]\n")
+	sys.stderr.write("usage: parse_trace.py <trace id> <start> <end> [--trace_archive_dir=traces] [--trace_server=localhost] [--download_only]\n")
 	sys.exit(1)
 
 traceid_base = sys.argv[1]
 start = int(sys.argv[2])
 end = int(sys.argv[3])
 
+print_summary = True
 load_from_trace_dir = False
 trace_archive_dir="traces"
 trace_server = "localhost"
@@ -26,6 +27,9 @@ for arg in sys.argv[4:]:
 	
 	if arg.startswith("--trace_server="):
 		trace_server = arg.replace("--trace_server=", "", 1)
+		
+	if arg.startswith("--download_only"):
+		print_summary = False
 
 if start < 0 or end < 0 or start > end:
 	sys.stderr.write("error: start must be less than or equal to end!\n")
@@ -73,28 +77,30 @@ for i in range(start, end+1):
 	else:
 		trace.loadFromFile(os.path.join(trace_archive_dir, traceid))
 
-	total_exec_time += trace.root.get_time()
-	total_db_stats.add(trace.root.get_database_stats())
+	if print_summary:
 
-	loc_cache_time += trace.root.get_time(["LocalisationCache::"])
-	loc_cache_db_stats.add(trace.root.get_database_stats(["LocalisationCache::"]))
-	loc_cache_cache_stats.add(trace.root.get_cache_stats(["LocalisationCache::"]))
+		total_exec_time += trace.root.get_time()
+		total_db_stats.add(trace.root.get_database_stats())
+
+		loc_cache_time += trace.root.get_time(["LocalisationCache::"])
+		loc_cache_db_stats.add(trace.root.get_database_stats(["LocalisationCache::"]))
+		loc_cache_cache_stats.add(trace.root.get_cache_stats(["LocalisationCache::"]))
 	
-	parser_time += trace.root.get_time(["Parser::"])
-	parser_db_stats.add(trace.root.get_database_stats(["Parser::", "ParserCache::"]))
-	parser_cache_stats.add(trace.root.get_cache_stats(["ParserCache::get"]))
+		parser_time += trace.root.get_time(["Parser::"])
+		parser_db_stats.add(trace.root.get_database_stats(["Parser::", "ParserCache::"]))
+		parser_cache_stats.add(trace.root.get_cache_stats(["ParserCache::get"]))
 	
-	resldr_time += trace.root.get_time(["ResourceLoader::"])
-	resldr_db_stats.add(trace.root.get_database_stats(["ResourceLoader::"]))
-	resldr_cache_stats.add(trace.root.get_cache_stats(["ResourceLoader::"]))
+		resldr_time += trace.root.get_time(["ResourceLoader::"])
+		resldr_db_stats.add(trace.root.get_database_stats(["ResourceLoader::"]))
+		resldr_cache_stats.add(trace.root.get_cache_stats(["ResourceLoader::"]))
 	
-	rev_time += trace.root.get_time(["Revision::"])
-	rev_db_stats.add(trace.root.get_database_stats(["Revision::"]))
-	rev_cache_stats.add(trace.root.get_cache_stats(["Revision::"]))
+		rev_time += trace.root.get_time(["Revision::"])
+		rev_db_stats.add(trace.root.get_database_stats(["Revision::"]))
+		rev_cache_stats.add(trace.root.get_cache_stats(["Revision::"]))
 	
-	linker_time += trace.root.get_time(["Linker::", "LinkCache::"])
-	linker_db_stats.add(trace.root.get_database_stats(["Linker::", "LinkCache::"]))
-	linker_cache_stats.add(trace.root.get_cache_stats(["Linker::", "LinkCache::"]))
+		linker_time += trace.root.get_time(["Linker::", "LinkCache::"])
+		linker_db_stats.add(trace.root.get_database_stats(["Linker::", "LinkCache::"]))
+		linker_cache_stats.add(trace.root.get_cache_stats(["Linker::", "LinkCache::"]))
 	
 	#print "Processed trace %d" % (i,)
 	progbar.update(j)
@@ -102,46 +108,43 @@ for i in range(start, end+1):
 	
 progbar.finish()	
 
-print
-print "Total Execution Time = %f" % (total_exec_time,)
-print
+if print_summary:
+	print
+	print "Total Execution Time = %f" % (total_exec_time,)
+	print
 
-print "Database Stats:"
-print total_db_stats
-print
+	print "Database Stats:"
+	print total_db_stats
+	print
 
-print "Localization Stats:"
-print "Time = %f" % (loc_cache_time, )
-print loc_cache_db_stats
-print loc_cache_cache_stats
-print 
+	print "Localization Stats:"
+	print "Time = %f" % (loc_cache_time, )
+	print loc_cache_db_stats
+	print loc_cache_cache_stats
+	print 
 
-print "Parser Stats:"
-print "Time = %f" % (parser_time, )
-print parser_db_stats
-print parser_cache_stats
-print
+	print "Parser Stats:"
+	print "Time = %f" % (parser_time, )
+	print parser_db_stats
+	print parser_cache_stats
+	print
 
-print "Resource Loader Stats:"
-print "Time = %f" % (resldr_time, )
-print resldr_db_stats
-print resldr_cache_stats
-print
+	print "Resource Loader Stats:"
+	print "Time = %f" % (resldr_time, )
+	print resldr_db_stats
+	print resldr_cache_stats
+	print
 
-print "Revision:"
-print "Time = %f" % (rev_time,)
-print rev_db_stats
-print rev_cache_stats
-print
+	print "Revision:"
+	print "Time = %f" % (rev_time,)
+	print rev_db_stats
+	print rev_cache_stats
+	print
 
-print "Linker:"
-print "Time = %f" % (linker_time, )
-print linker_db_stats
-print linker_cache_stats
-print
-
-
-
-#traces.save("trace_archive")
+	print "Linker:"
+	print "Time = %f" % (linker_time, )
+	print linker_db_stats
+	print linker_cache_stats
+	print
 
 sys.exit(0)
